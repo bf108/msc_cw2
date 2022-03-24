345796f2e1fce30b55182b2e9118c7823d6d90ab
Ben Farrell <ben.farrell@iqvia.com>
2022-03-24 20:30:21 +0000

    added doc strings to geo content

--- src/geo_etl/helper_functions.py
+++ src/geo_etl/helper_functions.py
@@ -10,6 +10,15 @@ from shapely.geometry import Point
 
 
 def find_centroid(array_min_lng, array_max_lng, array_min_lat, array_max_lat):
+    """
+    find centroid of bounding box given min/max lng/lat
+
+    :param array_min_lng: np.array (floats) - min longitude
+    :param array_max_lng: np.array (floats) - max longitude
+    :param array_min_lat: np.array (floats) - min latitude
+    :param array_max_lat: np.array (floats) - max latitude
+    :return: np.array( tuple(float, float) ) - lng, lat of centroid of bounding box
+    """
     lng = array_min_lng + (array_max_lng - array_min_lng) / 2
     lat = array_min_lat + (array_max_lat - array_min_lat) / 2
 
@@ -17,6 +26,18 @@ def find_centroid(array_min_lng, array_max_lng, array_min_lat, array_max_lat):
 
 
 def edges_leading_diaganol(array_min_lng, array_max_lng, array_min_lat, array_max_lat):
+    """
+    Converts min/max lng/lat of bounding box into two points.
+
+    bottom left hand corner (blhc) coordinates (lng, lat)
+    top right hand corner (trhc) coordinates (lng, lat)
+
+    :param array_min_lng: np.array (floats) - min longitude
+    :param array_max_lng: np.array (floats) - max longitude
+    :param array_min_lat: np.array (floats) - min latitude
+    :param array_max_lat: np.array (floats) - max latitude
+    :return:   np.array( tuple(float, float) ) - blhc coords, trhc coords of bounding box
+    """
     blhc = np.array((array_min_lng, array_min_lat)).T
     trhc = np.array((array_max_lng, array_max_lat)).T
 
@@ -24,6 +45,17 @@ def edges_leading_diaganol(array_min_lng, array_max_lng, array_min_lat, array_ma
 
 
 def leading_diag_len(df):
+    """
+    Adds a new column to DataFrame (df) of the leading diagongal length of location data.
+
+    It is necessary for the following columns to exists in df:
+
+    'place_longitude_1', 'place_longitude_2', 'place_latitude_1', 'place_latitude_2'
+
+    :param df: pd.DataFrame -
+    :return: df: pd.DataFrame - with additional column ['diagonal']
+                                which is the of leading diag length of the location marker
+    """
     cols = ['place_longitude_1', 'place_longitude_2', 'place_latitude_1', 'place_latitude_2']
 
     #Check all necessary columns present
@@ -34,14 +66,12 @@ def leading_diag_len(df):
     #Get edges of box
     edges_bbox = edges_leading_diaganol(*[df[c].values for c in cols])
 
-    #Create array of diagonal
     diagonal = []
     for blhc, trhc in zip(edges_bbox[0], edges_bbox[1]):
         try:
             ans = abs(np.linalg.norm(trhc - blhc))
         except:
             ans = np.nan
-
         diagonal.append(ans)
 
     df_diagonal = pd.DataFrame(diagonal, columns=['diagonal'])
@@ -50,6 +80,7 @@ def leading_diag_len(df):
 
 
 def calc_centroid_diag_bbox(df):
+    """Wrapper function to add location centroid and leading diagonal length as new columns of a df"""
     cols = ['place_longitude_1', 'place_longitude_2', 'place_latitude_1', 'place_latitude_2']
     centroid_loc_bbox = find_centroid(*[df[c].values for c in cols])
     df_loc_lat_centroid = pd.DataFrame(centroid_loc_bbox, columns=['loc_centroid_lng', 'loc_centroid_lat'])
@@ -81,8 +112,8 @@ def calc_centroid_diag_bbox(df):
     return df_tweets
 
 
-# This data is for the entire world, lets limit it to our bounding box for EUROPE
 def check_country_in_eur_bbox(row):
+    "Limits geometry to bounding box for EUROPE provided in question"
     long_min = -24.5
     long_max = 69.1
     lat_min = 34.8
